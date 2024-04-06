809799cd572bdfce44b6e75b36931f4bb60e880b
Shubham <bhattacharya.shubham123@gmail.com>
2024-04-06 18:18:59 -0500

    added button styling

--- src/MyApplication.js
+++ src/MyApplication.js
@@ -31,7 +31,7 @@ function MyApplication() {
       {viewer === 0 && <Browse handleSubmit={handleSubmit} updateHooks={updateHooks} onSubmit={onSubmit} register={register} errors={errors} />}
       {viewer === 1 && <Cart   handleSubmit={handleSubmit} onSubmit={onSubmit} errors={errors} dataF={dataF} updateHooks={updateHooks} register={register} />}
       {viewer === 2 && <Confirmation dataF={dataF} updateHooks={updateHooks} />}
-      <button onClick={decrementViewer} disabled={viewer === -1}>Return</button>
+      <button className="btn btn-primary" onClick={decrementViewer} disabled={viewer === -1}>Return</button>
     </div>
   );
 }
